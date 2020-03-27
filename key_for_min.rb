c6571c9849fc16be5330b99f4b23845cadbe9efd
CiaraMaria Proia <proiacm@gmail.com>
2020-03-27 01:27:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key = nil
-  value = 999
-  name_hash.collect do |name, age|
-    if age < value
-      value = age
-      name = key
+  low_key = nil
+  low_age = 999
+  name_hash.collect do |key, age|
+    if age < low_age
+      low_age = age
+      low_key = key
     end  
   end
-  key
+  low_key
 end
