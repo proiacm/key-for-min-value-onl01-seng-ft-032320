99586b0a389958e7e2a8e7d2f5b14b54a528527c
CiaraMaria Proia <proiacm@gmail.com>
2020-03-27 01:24:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   key = nil
   value = 999
-  name_hash.each do |name, age|
+  name_hash.collect do |name, age|
     if age < value
       value = age
       name = key
