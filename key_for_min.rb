bffbcc5fcb529ce5247a92e800d985e08246d7e9
CiaraMaria Proia <proiacm@gmail.com>
2020-03-27 01:04:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, age|
+  name_hash.each do |name, age|
   
     
   end
